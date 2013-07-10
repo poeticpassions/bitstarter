1a934eefaf13239352809995e90c71721b2d52a5
Ubuntu <ubuntu@ip-10-164-75-91.ec2.internal>
2013-07-10 03:36:41 +0000

    try to update web.js to read index.html

--- web.js
+++ web.js
@@ -5,6 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 fs.readFile('index.html','utf8', function (err,data) {
+if (err) throw err;`
+
 response.send(data);
 
 }
